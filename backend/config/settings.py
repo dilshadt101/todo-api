3e82b0b2f049bffead8ed7d0037836076ae127df
Mohammed Dilshad <dilshadt101@gmail.com>
2021-10-22 17:50:41 +0530

    created todo model

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 
+    'rest_framework',
+
+
     'todos',
 ]
 
@@ -121,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
